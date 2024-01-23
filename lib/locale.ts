78dba31c8852e9b906e7ce30ecc3619ef9787e44
bgSignpost <132521292+bgSignpost@users.noreply.github.com>
2024-01-23 16:11:43 -0500

    Update locale.ts

--- lib/locale.ts
+++ lib/locale.ts
@@ -13,26 +13,26 @@ export const LOCALES: { [key: string]: Locale } = {
     name: 'English',
     directus: 'en-US',
   },
-  // TODO
+  fr: { url: 'fr', direction: 'ltr', name: 'Français', directus: 'fr-FR' },
 };
 
 export const LOCALE_CODES_TO_CANONICAL_LOCALE_CODES: { [key: string]: string } =
   {
     en: 'en-us',
     'en-us': 'en-us',
-    // TODO
+    fr: 'fr',
   };
 
 // Returns the effective locale given locale code.
 //
 // This function defaults to en-us in case we ended up in a situation where the locale is not recognized. It's defensive programming as this shouldn't happen.
 export function getLocaleFromCode(code: string): Locale {
-  return LOCALES[code] ?? LOCALES['en-us'];
+  return LOCALES[code] ?? LOCALES['fr'];
 }
 
 /* Returns a Zendesk locale id for the current locale.
  If there is no mapping for the requested locale, return the deafault id 
  for en-us locale. */
 export const getZendeskLocaleId = (currentLocale: Locale): number => {
-  return DYNAMIC_CONTENT_LOCALES[currentLocale.url] || 1;
+  return DYNAMIC_CONTENT_LOCALES[currentLocale.url] || 16;
 };
