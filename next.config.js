f5ecaf6308591e83824dafd7a29c026a352d5b70
bgSignpost <132521292+bgSignpost@users.noreply.github.com>
2024-01-23 16:11:11 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -42,43 +42,41 @@ module.exports =  withLess({
     ...nextConfig,
     lessLoaderOptions: {
       lessOptions: {
-        // See full list of Ant styles here:
-        // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-        modifyVars: {// TODO: Update theme colors
-        'primary-color': '#51258F',
-        'secondary-color': '#854ECA',
-        'accent-color': '#13C2C2',
-        'info-color': '@accent-color',
-        'alert-info-bg-color': '@accent-color',
-        // Override Ant typography.
-        'heading-1-size': '3.75rem',
-        'heading-2-size': '1.875rem',
-        'typography-title-font-weight': '400',
-        // Set height and border radius for 'default' type elements, e.g. buttons, inputs, etc.
-        'height-base': '48px',
-        'height-sm': '32px',
-        'border-radius-base': '10px',
-        'border-color-base': '#141414',
-        // Set header styles.
-        'layout-header-height': 'auto',
-        'layout-header-min-height': '4.375rem',
-        'layout-header-padding': '0',
-        'header-background-color': '@primary-color',
-        'header-text-color': 'white',
-        'header-banner-background-color':
-          'linear-gradient(@secondary-color, @primary-color)',
-        'header-banner-text-color': 'white',
-        // Set search styles.
-        'search-icon-color': 'black',
-        'search-icon-bg-color': '@accent-color',
-        // Set card styles.
-        'card-padding-base': '16px',
-        'home-page-card-icon-color': '#141414',
-        // Set cookie banner styles.
-        'cookie-banner-text': '#000',
-        'cookie-banner-back': '@accent-color',
-        'link-color': '#0000ee',
-        'link-hover-color': '#0000eea8',
+        modifyVars: {//
+          'primary-color': '#38393C',
+          'secondary-color': '#242629',
+          'accent-color': '#FFB500',
+          'info-color': '@accent-color',
+          'alert-info-bg-color': '@accent-color',
+          // Override Ant typography.
+          'heading-1-size': '3.75rem',
+          'heading-2-size': '1.875rem',
+          'typography-title-font-weight': '400',
+          // Set height and border radius for 'default' type elements, e.g. buttons, inputs, etc.
+          'height-base': '48px',
+          'height-sm': '32px',
+          'border-radius-base': '10px',
+          'border-color-base': '#141414',
+          // Set header styles.
+          'layout-header-height': 'auto',
+          'layout-header-min-height': '4.375rem',
+          'layout-header-padding': '0',
+          'header-background-color': '@secondary-color 8.14%',
+          'header-text-color': 'white',
+          'header-banner-background-color':
+            'linear-gradient(173.7deg, @secondary-color 8.14%, @primary-color 127.84%)',
+          'header-banner-text-color': 'white',
+          // Set search styles.
+          'search-icon-color': 'white',
+          'search-icon-bg-color': '@accent-color',
+          // Set card styles.
+          'card-padding-base': '16px',
+          'home-page-card-icon-color': '#141414',
+          // Set cookie banner styles.
+          'cookie-banner-text': '#000',
+          'cookie-banner-back': '@accent-color',
+          'link-color': '#0000ee',
+          'link-hover-color': '#0000eea8',
         },
       },
     },
